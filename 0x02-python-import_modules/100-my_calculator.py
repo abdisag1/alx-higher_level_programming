dc625b75329ad8a0b8daa096ba12c0a47eb2e28e
Abdisa Gemechu <myabdisa@gmail.com>
2021-11-24 16:40:37 -0800

    added task 0-6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":  
     import sys
-
-    if len(sys.argv) - 1 != 3:
+    from calculator_1 import add, sub, mul, div
+    n= (sys.argv)
+    arglen = (len(sys.argv))-1
+    if arglen != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    op = sys.argv[2]
+    oplist =["+","-","*","/"]
+    opcheck = op in oplist
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
+       
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+   # <a> <operator> <b> = <result>  a =b = oper(a,b)
+    if (op == oplist[0]):
+        result = add(a,b)
+    elif (op == oplist[1]):
+        result = sub(a, b)
+    elif(op == oplist[2]):
+        result = mul(a, b)
+    elif(op == oplist[3]):
+        result = div(a,b)
+    else:
+         print("Unknown operator. Available operators: +, -, * and")
+    
+         exit(1)
+    print("{} {} {} = {}".format(a, op, b, result))
