c5112845ee5de2e2e6b3f5ff97384cefe2440aa6
abdisag1 <myabdisa@gmail.com>
2021-12-31 16:56:36 +0000

    done the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Base class
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        with public attribute id
+        :param id:
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
