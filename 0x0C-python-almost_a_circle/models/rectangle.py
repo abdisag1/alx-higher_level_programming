506a7c4856949007da0e90cdc816aef23d29b81b
abdisag1 <myabdisa@gmail.com>
2022-01-03 07:48:05 +0000

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,3 +89,29 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}"
                 .format(self.id, self.__x, self.__y, self.__width, self.__height))
 
+    def update(self, *args, **kwargs):
+        for arg in args:
+            i = args.index(arg)
+            while i < len(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.__width = arg
+                elif i == 2:
+                    self.__height = arg
+                elif i == 3:
+                    self.__x = arg
+                else:
+                    self.__y = arg
+                i += 1
+        for key, value in kwargs.items():
+            if key == "height":
+                self.__height = value
+            elif key == "width":
+                self.__width = value
+            elif key == "x":
+                self.__x = value
+            elif key == "y":
+                self.__y = value
+            elif key == "id":
+                self.id = value
