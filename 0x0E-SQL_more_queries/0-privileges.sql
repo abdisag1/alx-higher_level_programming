063976bead1cafe289a648372aac411d48288f4f
abdisag1 <myabdisa@gmail.com>
2022-02-02 17:31:46 +0000

    done task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
---Lists all priveleges of the MYsql user `user_0d_1` and `user_0d_2` in `localhost`.
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_od_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
