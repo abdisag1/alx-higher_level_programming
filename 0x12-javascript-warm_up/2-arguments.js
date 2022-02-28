aea7ca614ab336668d6884e203ffd5c57d7761e1
abdisag1 <myabdisa@gmail.com>
2022-02-28 18:46:41 +0000

    modifed 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-command = process.argv;
-if (command != null)
-console.log("Argument found");
-else 
-console.log("NO Argument");
+let number_of_arg = process.argv.length;
+if (number_of_arg === 2) {
+  console.log('No argument');
+} else if (number_of_arg === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
