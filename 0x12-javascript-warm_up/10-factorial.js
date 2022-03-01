0fee4ef550fa8f4b7deabcb74d0b7d21af1f756a
abdisag1 <myabdisa@gmail.com>
2022-03-01 02:12:24 +0000

    fixed semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let first_arg = process.argv[2];
-function factorial (first_arg) {
-    if (isNaN(first_arg) || first_arg === 1) {
-	return (1);
-    } else {
-	return (first_arg * factorial(first_arg-1));
-    }
+const firstArg = process.argv[2];
+function factorial (firstArg) {
+  if (isNaN(firstArg) || firstArg === 1) {
+    return (1);
+  } else {
+    return (firstArg * factorial(firstArg - 1));
+  }
 }
-console.log(factorial(parseInt(first_arg)));
+console.log(factorial(parseInt(firstArg)));
