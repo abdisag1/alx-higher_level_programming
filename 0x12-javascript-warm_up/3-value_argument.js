402577ff486bc3c1e54619db9187b117ba7d75f0
abdisag1 <myabdisa@gmail.com>
2022-03-01 02:35:21 +0000

    fixed semistandard

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 'use strict';
-let arguments = process.argv;
-if (arguments[2] === undefined)
-console.log('No argument');
-else {
-console.log(arguments[2]);
+const arg = process.argv[2];
+if (arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg);
 }
