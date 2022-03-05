afcb51a5e7402cf58948299d20e20cfe27a68ad4
abdisag1 <myabdisa@gmail.com>
2022-03-05 08:19:08 +0000

    done task 1 script that lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+
 """
 script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 """
-
 
 import MySQLdb
 from sys import argv
 
 
-if __name__ ==  "__main__":
-    db_conn = MySQLdb.connect(host="localhost", port=3306, user = argv[1], passwd= argv[2],db =argv[3])
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(host="localhost", user = argv[1], passwd= argv[2],db =argv[3])
     cursor = db_conn.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE (name LIKE 'N%' AND id<=5) ORDER BY id")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
     db_conn.close()
+    
