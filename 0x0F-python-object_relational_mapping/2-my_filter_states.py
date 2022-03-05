5926a0431debee3a04e95a0ff0142eae176545cb
abdisag1 <myabdisa@gmail.com>
2022-03-05 09:19:16 +0000

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states with a given name
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_conn = MySQLdb.connect(host="localhost", user = argv[1], passwd = argv[2],db = argv[3])
+    searched = argv[4]
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM states WHERE (name = '{}' AND id <3) ORDER BY id".format(searched))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db_conn.close()
