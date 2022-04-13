ca0a812ebb67a49f30c10ec0751b9caa16fed697
abdisag1 <myabdisa@gmail.com>
2022-04-13 14:26:58 +0000

    used get method

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response) {
+require('request').get(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code:' + response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
